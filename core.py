d2c40f5f6854f6f80c7553e137d04783666f6907
Burdi14 <lv_ant2018@mail.ru>
2024-12-09 16:28:52 +0300

    final version

--- core.py
+++ core.py
@@ -6,6 +6,7 @@ import json
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
+#getting the location key from API function
 def get_location_key(city: str) -> str:
     url = "http://dataservice.accuweather.com/locations/v1/cities/search"
     params = {
@@ -19,10 +20,11 @@ def get_location_key(city: str) -> str:
         print("For today the amount of API requests was exceeded")
     data = response.json()
     try:
-        return data[0]['Key']
+        return data[0]['Key'], data[0]['LocalizedName']
     except:
         raise ValueError("Location key whas not found")
 
+#getting forecast for the next 5 days (only tempreture 'cause of Accuwather api)
 def get_5day_forecast(location_key: str):
     url = f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}"
     params = {
@@ -36,14 +38,3 @@ def get_5day_forecast(location_key: str):
     response.raise_for_status()
 
     return response.json()["DailyForecasts"]
-
-def get_current_weather(location_key: str):    
-    url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={API_KEY}&details=true"
-    response = requests.get(url)
-    print(response)
-    if response.status_code == 503:
-        print("For today the amount of API requests was exceeded")
-    response.raise_for_status()
-    with open("weath.json", "w") as f:
-        json.dump(response.json(), f, indent = 4)
-    return response.json()
